7ef2bdccc11e407eb2190eb2a8052122204b85cb
hawkgs <hawk.dvp@gmail.com>
2018-10-13 12:41:44 +0300

    Add projects

--- src/site/projects/shared/project/Project.js
+++ src/site/projects/shared/project/Project.js
@@ -5,8 +5,18 @@ import html from './Project.html';
 import css from './Project.css';
 
 export class Project extends Component {
+  static get observedAttributes() {
+    return ['name', 'description'];
+  }
+
   constructor() {
     super(html, [css]);
+  }
+
+  onComponentAttach() {
+    this.root.querySelector('.project').href = this.attr.url || '/';
+    this.root.querySelector('.name').innerHTML = this.attr.name || 'N/A';
+    this.root.querySelector('.descr').innerHTML = this.attr.description || 'N/A';
   }
 }
 
