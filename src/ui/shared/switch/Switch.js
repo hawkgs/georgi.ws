b4483927b5db79b42e17b4f1e65a1508af078f37
hawkgs <hawk.dvp@gmail.com>
2019-10-08 20:25:02 +0300

    Throw change events on switch on/off

--- src/ui/shared/switch/Switch.js
+++ src/ui/shared/switch/Switch.js
@@ -6,13 +6,20 @@ import css from './Switch.css';
 export class Switch extends Component {
   constructor() {
     super('<div class="switch"></div>', [css]);
+    this._on = false;
   }
 
   onComponentAttach() {
     const switchCont = this.root.querySelector('.switch');
 
     switchCont.addEventListener('click', () => {
+      this._on = !this._on;
       switchCont.classList.toggle('on');
+
+      const event = new CustomEvent('change', {
+        detail: { on: this._on }
+      });
+      this.dispatchEvent(event);
     });
   }
 }
