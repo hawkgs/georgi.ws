391c1f585505a978c81d86c5e97452112b82182f
hawkgs <hawk.dvp@gmail.com>
2019-10-08 21:16:39 +0300

    Add support for initial value for the switch

--- src/ui/shared/switch/Switch.js
+++ src/ui/shared/switch/Switch.js
@@ -3,24 +3,40 @@
 import { Component } from '../../../core/component';
 import css from './Switch.css';
 
+const ON_CLASS = 'on';
+
 export class Switch extends Component {
+  static get observedAttributes() {
+    return ['on'];
+  }
+
   constructor() {
     super('<div class="switch"></div>', [css]);
     this._on = false;
   }
 
   onComponentAttach() {
+    this._on = this.attr.on === 'true';
     const switchCont = this.root.querySelector('.switch');
+
+    if (this._on) {
+      switchCont.classList.add(ON_CLASS);
+    }
 
     switchCont.addEventListener('click', () => {
       this._on = !this._on;
-      switchCont.classList.toggle('on');
 
-      const event = new CustomEvent('change', {
-        detail: { on: this._on }
-      });
-      this.dispatchEvent(event);
+      switchCont.classList.toggle(ON_CLASS);
+      this.setAttribute('on', this._on);
+      this._emitEvent(this._on);
     });
+  }
+
+  _emitEvent(on) {
+    const event = new CustomEvent('change', {
+      detail: { on }
+    });
+    this.dispatchEvent(event);
   }
 }
 
